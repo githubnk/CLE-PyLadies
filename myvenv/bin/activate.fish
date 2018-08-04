bf9cd6722cc6f0c14567e5a6edd692886cc928bb
mutterberg <Marissa@UtterbergDataDev.com>
2018-08-04 10:24:42 -0400

    Changing virtual environment to functional local dir

--- myvenv/bin/activate.fish
+++ myvenv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/NothingToLose/pyladies/myvenv"
+set -gx VIRTUAL_ENV "/Users/NothingToLose/CLE-PyLadies/myvenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- myvenv/bin/django-admin.py
+++ myvenv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/Users/NothingToLose/pyladies/myvenv/bin/python
+#!/Users/NothingToLose/CLE-PyLadies/myvenv/bin/python
 from django.core import management
 
 if __name__ == "__main__":
